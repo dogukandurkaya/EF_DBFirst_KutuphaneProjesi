fb49dbd6e38fb126e107c6cf94563458dc470bd0
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2022-06-14 14:15:30 +0300

    Initial commit

---
+++ EF_03_KutuphaneProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EF_03_KutuphaneProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
